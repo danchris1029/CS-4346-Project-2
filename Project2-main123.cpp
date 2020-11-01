66c45240740b4076a262e1edca45f86acacae285
John <35585496+CyanHawkk@users.noreply.github.com>
2020-10-31 23:46:22 -0500

    Update Project2-main123.cpp

--- Project2-main123.cpp
+++ Project2-main123.cpp
@@ -108,3 +108,25 @@ int cornAndMid(vector<int> currentState, int player) {
 
 	return score;
 }
+
+int proximityEval(vector<int> currentState, int player){
+    int scorePlayer = 0;
+    int scoreOtherPlayer = 0;
+    
+    // Checking proximity of values above each player
+    for(int index = 3; index < 9; index++){
+        if (currentstate[index - 3] == player && currentstate[index - 3] == (-player))
+            scorePlayer += 2;
+    }
+    
+    // Checking proximity of values above each other player
+    for(int index = 3; index < 9; index++){
+        if (currentstate[index - 3] == (-player) && currentstate[index - 3] == player)
+            scoreOtherPlayer += 2;
+    }
+    
+    return(scorePlayer - scoreOtherPlayer);
+    
+}
+ // -1 = O
+ // 1 = X
